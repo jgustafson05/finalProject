ea9e91b2142817d125c788148a5e86ee7bce001b
Bryce <bryceis2@illinois.edu>
2019-11-26 13:32:53 -0600

    Will now create a file with the survey title if the name has not been used already.

--- app/src/main/java/com/example/finalproject/LaunchActivity.java
+++ app/src/main/java/com/example/finalproject/LaunchActivity.java
@@ -6,9 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
+import java.io.IOException;
 
 /** Requires and completes creation or loading of a new file. */
 public class LaunchActivity extends AppCompatActivity {
@@ -21,38 +25,54 @@ public class LaunchActivity extends AppCompatActivity {
     Button createFile = findViewById(R.id.createFile);
     Button loadFile = findViewById(R.id.loadFile);
 
-    createFile.setOnClickListener(unused -> {
-      AlertDialog.Builder builder = new AlertDialog.Builder(this);
-      builder.setView(R.layout.dialog_newfile);
-
-      // Making a dialog to request title for survey
-      builder.setTitle(R.string.dialog_title)
-          .setPositiveButton(R.string.dialog_create, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(final DialogInterface dialog, final int which) {
-              Dialog dialogView = (Dialog) dialog;
-              TextView dialogPrompt = dialogView.findViewById(R.id.dialogPrompt);
-              if (dialogPrompt.getText().toString().equals("")) {
-                //Alert user somehow
-                return;
-              }
-              Intent intent = new Intent(getApplicationContext(), NewSurveyActivity.class);
-              Bundle bundle = new Bundle();
-              bundle.putString("title", dialogPrompt.getText().toString());
-              //Add to intent here
-
-              intent.putExtras(bundle);
-              startActivity(intent);
-              finish();
-              }
-          })
-              .setNegativeButton(R.string.dialog_cancel, new DialogInterface.OnClickListener() {
-                  @Override
-                  public void onClick(final DialogInterface dialog, final int which) {
-                  }
-              });
-      builder.show();
-    });
+    createFile.setOnClickListener(unused -> createNewFileDialog());
     loadFile.setOnClickListener(unused -> { });
   }
+
+  private void createNewFileDialog() {
+    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+    builder.setView(R.layout.dialog_newfile);
+
+    // Making a dialog to request title for survey
+    builder.setTitle(R.string.dialog_title)
+           .setPositiveButton(R.string.dialog_create, new DialogInterface.OnClickListener() {
+             @Override
+             public void onClick(final DialogInterface dialog, final int which) {
+               Dialog dialogView = (Dialog) dialog;
+               TextView dialogPrompt = dialogView.findViewById(R.id.dialogPrompt);
+               if (dialogPrompt.getText().toString().equals("")) {
+                 //Alert user somehow
+                 return;
+               }
+               try {
+                 File surveyFile = new File(getApplicationContext().getFilesDir(),
+                           dialogPrompt.getText().toString());
+                 //Need to check for invalid file names here I think
+                 if (!surveyFile.createNewFile()) {
+                   //Alert the user
+                   return;
+                 }
+               } catch (IOException e) {
+                 Toast ioProblem = Toast.makeText(getApplicationContext(),
+                         "Error: not enough space to make a file", Toast.LENGTH_LONG);
+                 ioProblem.show();
+               }
+               Intent intent = new Intent(getApplicationContext(), NewSurveyActivity.class);
+               Bundle bundle = new Bundle();
+               bundle.putString("title", dialogPrompt.getText().toString());
+               //Add to intent here
+
+               intent.putExtras(bundle);
+               startActivity(intent);
+               finish();
+             }
+           })
+           .setNegativeButton(R.string.dialog_cancel, new DialogInterface.OnClickListener() {
+             @Override
+             public void onClick(final DialogInterface dialog, final int which) {
+             }
+           });
+    builder.show();
+  }
+
 }
--- app/src/main/java/com/example/finalproject/NewSurveyActivity.java
+++ app/src/main/java/com/example/finalproject/NewSurveyActivity.java
@@ -1,18 +1,37 @@
 package com.example.finalproject;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.File;
+import java.util.ArrayList;
 
 
 public class NewSurveyActivity extends AppCompatActivity {
+
+  ArrayList<Variable> variables = new ArrayList<>();
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_new_survey);
 
-    
+    //Need to change layout based on screen size
+
+    String surveyTitle = getIntent().getStringExtra("title");
+    File surveyFile = new File(getApplicationContext().getFilesDir(), surveyTitle);
+
+    Button newVariable = findViewById(R.id.newVariable);
+    Button createSurvey = findViewById(R.id.createSurvey);
+
+    newVariable.setOnClickListener(unused -> createNewVariableDialog());
   }
+
+  private void createNewVariableDialog() {
+
+  }
+
 }
